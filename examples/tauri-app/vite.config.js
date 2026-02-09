63f95f7ecbc3f8588a09bdd09ed8799e1fe9d6cc
Erle Carrara <ecarrara@users.noreply.github.com>
2026-02-09 22:23:15 +0000

    Fix Vite config: set root to src directory

--- examples/tauri-app/vite.config.js
+++ examples/tauri-app/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'src',
   clearScreen: false,
   server: {
     strictPort: true,
   },
   envPrefix: ['VITE_', 'TAURI_'],
   build: {
+    outDir: '../dist',
+    emptyOutDir: true,
     target: 'esnext',
     minify: !process.env.TAURI_DEBUG,
     sourcemap: !!process.env.TAURI_DEBUG,
